a407a9f738525867cf2b59a3b4c19b69ebd42468
417yuri <56441504+YuriSuzuki0901@users.noreply.github.com>
2023-06-18 23:38:50 +0900

    ダミーデータ生成等

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(User::class)->create([
+            'name' => '椎名林檎',
+            'email' => 'test@test.test',
+            'email_verified_at' => now(),
+            'password' => Hash::make('testtest'),
+        ]);
+
+        User::factory(10)->create();
+    }
+}
