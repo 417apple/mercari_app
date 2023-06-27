21c77bf6d47e5d321c1f9164f2430d180cd23fa9
417yuri <56441504+YuriSuzuki0901@users.noreply.github.com>
2023-06-28 01:48:09 +0900

    商品カテゴリー選択の実装

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MainCategory;
 use App\Models\ItemCondition;
 
 class SellController extends Controller
 {
     public function showSellForm()
      {
+        $categories = MainCategory::orderBy('sort_no')->get();
         $conditions = ItemCondition::orderBy('sort_no')->get();
-        return view('sell')->with('conditions', $conditions);
+        return view('sell', compact('categories', 'conditions'));
      }
 }
--- app/Models/MainCategory.php
+++ app/Models/MainCategory.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class MainCategory extends Model
 {
     use HasFactory;
+
+    public function subCategories()
+    {
+        return $this->hasMany(SubCategory::class);
+    }
 }
